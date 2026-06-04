e9eb6ec1d8062203c36816382f8866dd905a4841
kartik-lamkhade <kartiklamkhade7@gmail.com>
2026-06-04 16:59:02 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -7,9 +7,10 @@ uploaded_file = st.camera_input("Take a picture")
 if uploaded_file is not None:
     with st.spinner("Analyzing the image..."):
         response = requests.post("https://cure-done.onrender.com/analyze-wheat",files={"Upload_file": (uploaded_file.name, uploaded_file, "image/jpeg")})
-    try:
+    try: 
         st.write(f"**diagnosis:** {response.json()['diagnosis']}")
         st.write(f"**product:** {response.json()['product']}")
         st.write(f"**amazon_link:** {response.json()['amazon_link']}")
     except Exception as e:
-        st.error(e)
+        st.error("An error occurred while processing the image. Please try again.")
+        st.error(str(e))
